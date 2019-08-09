e027cf7f833ea643878a5f7c3654f2ee55c5983b
natan.daniel <natan.daniel0@gmail.com>
2019-08-10 01:25:03 +0200

    adding optimised bubble sort test

--- sorting_algorithms/src/sort/bubble/BubbleSortTest.java
+++ sorting_algorithms/src/sort/bubble/BubbleSortTest.java
@@ -1,6 +1,12 @@
 package sort.bubble;
 
-import static sort.common.Tools.*;
+import static sort.common.Tools.ARRAY_IN_ASCENDING_ORDER_CASE;
+import static sort.common.Tools.ARRAY_IN_DESCENDING_ORDER_CASE;
+import static sort.common.Tools.ARRAY_IN_RANDOM_ORDER_CASE;
+import static sort.common.Tools.BUBBLE_SORT;
+import static sort.common.Tools.OPT_BUBBLE_SORT;
+import static sort.common.Tools.getData;
+import static sort.common.Tools.sort;
 
 import java.util.Map;
 
@@ -13,12 +19,13 @@ public class BubbleSortTest {
 		Map<String, Integer[]> data = getData(50);
 
 		Sort bubbleSort = new BubbleSort();
+		Sort optBubbleSort = new OptimisedBubbleSort();
 
 		sort(BUBBLE_SORT, bubbleSort, data.get(ARRAY_IN_DESCENDING_ORDER_CASE), ARRAY_IN_DESCENDING_ORDER_CASE);
-		sort(OPT_BUBBLE_SORT, bubbleSort, data.get(ARRAY_IN_DESCENDING_ORDER_CASE), ARRAY_IN_DESCENDING_ORDER_CASE);
+		sort(OPT_BUBBLE_SORT, optBubbleSort, data.get(ARRAY_IN_DESCENDING_ORDER_CASE), ARRAY_IN_DESCENDING_ORDER_CASE);
 		sort(BUBBLE_SORT, bubbleSort, data.get(ARRAY_IN_RANDOM_ORDER_CASE), ARRAY_IN_RANDOM_ORDER_CASE);
-		sort(OPT_BUBBLE_SORT, bubbleSort, data.get(ARRAY_IN_RANDOM_ORDER_CASE), ARRAY_IN_RANDOM_ORDER_CASE);
+		sort(OPT_BUBBLE_SORT, optBubbleSort, data.get(ARRAY_IN_RANDOM_ORDER_CASE), ARRAY_IN_RANDOM_ORDER_CASE);
 		sort(BUBBLE_SORT, bubbleSort, data.get(ARRAY_IN_ASCENDING_ORDER_CASE), ARRAY_IN_ASCENDING_ORDER_CASE);
-		sort(OPT_BUBBLE_SORT, bubbleSort, data.get(ARRAY_IN_ASCENDING_ORDER_CASE), ARRAY_IN_ASCENDING_ORDER_CASE);
+		sort(OPT_BUBBLE_SORT, optBubbleSort, data.get(ARRAY_IN_ASCENDING_ORDER_CASE), ARRAY_IN_ASCENDING_ORDER_CASE);
 	}
 }
