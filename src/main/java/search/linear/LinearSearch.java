4c36a4c5522a9c79e3742ae2cb0996c106ac2069
natandaniel <natan.daniel0@gmail.com>
2022-03-03 10:17:05 +0100

    updating linear search
    Change-Id: Id70e4c879cdad3237d786120310f74a145550a15

--- src/main/java/search/linear/LinearSearch.java
+++ src/main/java/search/linear/LinearSearch.java
@@ -1,30 +1,19 @@
 package search.linear;
 
+import java.util.Objects;
 import java.util.OptionalInt;
 
-public class LinearSearch {
+class LinearSearch {
 
-	private LinearSearch() {
-	}
+  private LinearSearch() {}
 
-	/**
-	 * Returns the index of the specified value within the specified integer array
-	 * if present, an empty optional otherwise.
-	 * 
-	 * @param value an integer value to search within the specified integer array
-	 * @param input the integer array in which to search for the specified value
-	 * @return an optional with the index of the specified value within the
-	 *         specified integer array if present, an empty optional otherwise.
-	 */
-	public static OptionalInt search(int value, int[] input) {
-		if (input != null) {
-			for (int i = 0; i < input.length; i++) {
-				if (input[i] == value)
-					return OptionalInt.of(i);
-			}
-		}
+  static OptionalInt search(double value, double[] input) {
+    Objects.requireNonNull(input);
 
-		return OptionalInt.empty();
-	}
+    for (int i = 0; i < input.length; i++)
+      if (input[i] == value) return OptionalInt.of(i);
+
+    return OptionalInt.empty();
+  }
 
 }
--- src/test/java/search/linear/LinearSearchTest.java
+++ src/test/java/search/linear/LinearSearchTest.java
@@ -3,24 +3,24 @@ package search.linear;
 import java.util.OptionalInt;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class LinearSearchTest {
-	private static int[] INPUT = new int[30];
+  private static double[] INPUT = new double[30];
 
-	static {
-		for (int i = 0; i < INPUT.length; i++)
-			INPUT[i] = i + 1;
-	}
+  static {
+    for (int i = 0; i < INPUT.length; i++)
+      INPUT[i] = i + 1;
+  }
 
-	@Test
-	public void Given_IntegersFrom1To30_When_SearchingForValue24_Then_ReturnIndex23() {
-		Assert.assertEquals(OptionalInt.of(23), LinearSearch.search(24, INPUT));
-	}
+  @Test
+  public void Given_IntegersFrom1To30_When_SearchingForValue24_Then_ReturnIndex23() {
+    Assert.assertEquals(OptionalInt.of(23), LinearSearch.search(24, INPUT));
+  }
 
-	@Test
-	public void Given_IntegersFrom1To30_When_SearchingForValue31_Then_ReturnOptionalEmpty() {
-		Assert.assertEquals(OptionalInt.empty(), LinearSearch.search(31, INPUT));
-	}
+  @Test
+  public void Given_IntegersFrom1To30_When_SearchingForValue31_Then_ReturnOptionalEmpty() {
+    Assert.assertEquals(OptionalInt.empty(), LinearSearch.search(31, INPUT));
+  }
 
 }
