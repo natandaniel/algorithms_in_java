cb2d2c9aa4233cc427013b28e8021cc2d0728f95
natandaniel <natan.daniel0@gmail.com>
2022-03-03 08:49:29 +0100

    updating insertion sort
    Change-Id: I951d0bd97f6fc73fd44e2a3a6a7bf1da2498ee74

--- src/main/java/sort/insertion/InsertionSort.java
+++ src/main/java/sort/insertion/InsertionSort.java
@@ -2,34 +2,18 @@ package sort.insertion;
 
 import java.util.Objects;
 
-/**
- * Provides an implementation of the insertion sort algorithm applied to integers.</br>
- * Values can be sorted in both ascending and descending orders.
- * <p>
- * Time complexity : between O(N) (best case) and O(N^2) (worst case).
- */
 class InsertionSort {
 
   private InsertionSort() {}
 
-  /**
-   * Sorts the input array of integers in ascending or descending order.
-   * 
-   * @param input
-   *          an array of integers, must not be null
-   * @param isSortInAscendingOrder
-   *          if true, sorts the input array in ascending order, otherwise in descending order
-   * @throws NullPointerException
-   *           if input is null
-   */
-  static void sort(int[] input, boolean isSortInAscendingOrder) {
+  static void sort(int[] input) {
     Objects.requireNonNull(input);
 
     for (int j = 1; j < input.length; j++) {
       int key = input[j];
       int i = j - 1;
 
-      while (-1 < i && (isSortInAscendingOrder ? key < input[i] : input[i] < key))
+      while (-1 < i && key < input[i])
         input[i + 1] = input[i--];
 
       input[i + 1] = key;
--- src/test/java/sort/insertion/InsertionSortTest.java
+++ src/test/java/sort/insertion/InsertionSortTest.java
@@ -6,42 +6,35 @@ import org.junit.Test;
 public class InsertionSortTest {
 
   @Test
-  public void Given_ANullArray_When_InsertionSorting_Then_ShouldThrowNullPointerException() {
-    Assert.assertThrows(NullPointerException.class, () -> InsertionSort.sort(null, true));
+  public void Given_ANullArray_When_Sorting_Then_ShouldThrowNullPointerException() {
+    Assert.assertThrows(NullPointerException.class, () -> InsertionSort.sort(null));
   }
 
   @Test
-  public void Given_ASingleInteger_When_InsertionSorting_Then_ShouldGetInteger() {
+  public void Given_ASingleInteger_When_Sorting_Then_ShouldGetInteger() {
     int[] input = new int[] { 1 };
-    InsertionSort.sort(input, true);
+    InsertionSort.sort(input);
     Assert.assertArrayEquals(new int[] { 1 }, input);
   }
 
   @Test
-  public void Given_Integers10To1_When_InsertionSortingInAscendingOrder_Then_ShouldGetIntegers1To10() {
+  public void Given_Integers10To1_When_Sorting_Then_ShouldGetIntegers1To10() {
     int[] input = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-    InsertionSort.sort(input, true);
+    InsertionSort.sort(input);
     Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, input);
   }
 
   @Test
-  public void Given_Integers1To10_When_InsertionSortingInDescendingOrder_Then_ShouldGetIntegers10To1() {
+  public void Given_Integers1To10_When_Sorting_Then_ShouldGetIntegers1To10() {
     int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    InsertionSort.sort(input, false);
-    Assert.assertArrayEquals(new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, input);
-  }
-
-  @Test
-  public void Given_Integers1To10_When_InsertionSortingInAscendingOrder_Then_ShouldGetIntegers1To10() {
-    int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    InsertionSort.sort(input, true);
+    InsertionSort.sort(input);
     Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, input);
   }
 
   @Test
-  public void Given_10IntegersInRandomOrder_When_InsertionSortingInAscendingOrder_Then_ShouldRearrangeIntegersInAscendingOrder() {
+  public void Given_10IntegersInRandomOrder_When_Sorting_Then_ShouldGetIntegers1To10() {
     int[] input = new int[] { 10, 1, 9, 2, 8, 3, 7, 4, 6, 5 };
-    InsertionSort.sort(input, true);
+    InsertionSort.sort(input);
     Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, input);
   }
 
