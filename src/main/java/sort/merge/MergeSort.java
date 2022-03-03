e9fab5eaa509bcbbd23a649984be76f81dfa091c
natandaniel <natan.daniel0@gmail.com>
2022-03-03 08:55:46 +0100

    updating merge sort
    Change-Id: If8a57d82f78b7a510d4445412abbaa5804732dee

--- src/main/java/sort/merge/MergeSort.java
+++ src/main/java/sort/merge/MergeSort.java
@@ -2,60 +2,25 @@ package sort.merge;
 
 import java.util.Objects;
 
-/**
- * Provides an implementation of the merge sort algorithm applied to an array of integers.</br>
- * Values can be sorted in both ascending and descending orders.
- * <p>
- * For an array of size N :
- * <ul>
- * <li>the time complexity is : O(N*log(N))</li>
- * <li>the space complexity is : O(N)</li>
- * </ul>
- */
 class MergeSort {
 
   private MergeSort() {}
 
-  /**
-   * Sorts the input array of integers in ascending or descending order.
-   * 
-   * @param input
-   *          an array of integers, must not be null
-   * @param isSortInAscendingOrder
-   *          if true, sorts the input array in ascending order, otherwise in descending order
-   * @throws NullPointerException
-   *           if input is null
-   */
-  static void sort(int[] input, boolean isSortInAscendingOrder) {
+  static void sort(int[] input) {
     Objects.requireNonNull(input);
-
-    if (input.length <= 1) return;
-
-    mergeSort(input, 0, input.length - 1, isSortInAscendingOrder);
+    mergeSort(input, 0, input.length - 1);
   }
 
-  /**
-   * Does an in place merge sort of the specified input array from the specified start index to the end index.
-   * 
-   * @param input
-   *          an array of integers, must not be null and must contain elements ranging from the specified start and end indices
-   * @param startIndex
-   *          an index defining the starting index of the range of numbers to sort, must be within the specified array's bounds
-   * @param endIndex
-   *          an index defining the ending index of the range of numbers to sort, must be within the specified array's bounds
-   * @param isSortInAscendingOrder
-   *          if true, sorts the input array in ascending order, otherwise in descending order
-   */
-  private static void mergeSort(int[] input, int startIndex, int endIndex, boolean isSortInAscendingOrder) {
+  private static void mergeSort(int[] input, int startIndex, int endIndex) {
     if (startIndex < endIndex) {
       int midddleIndex = (startIndex + endIndex) / 2;
-      mergeSort(input, startIndex, midddleIndex, isSortInAscendingOrder);
-      mergeSort(input, midddleIndex + 1, endIndex, isSortInAscendingOrder);
-      merge(input, startIndex, midddleIndex, endIndex, isSortInAscendingOrder);
+      mergeSort(input, startIndex, midddleIndex);
+      mergeSort(input, midddleIndex + 1, endIndex);
+      merge(input, startIndex, midddleIndex, endIndex);
     }
   }
 
-  private static void merge(int[] input, int startIndex, int middleIndex, int endIndex, boolean isSortInAscendingOrder) {
+  private static void merge(int[] input, int startIndex, int middleIndex, int endIndex) {
     int leftLength = middleIndex - startIndex + 1;
     int[] left = new int[leftLength];
 
@@ -78,7 +43,7 @@ class MergeSort {
         if (rightIndex < rightLength) {
           int currentRightValue = right[rightIndex];
 
-          if (isSortInAscendingOrder ? currentLeftValue <= currentRightValue : currentLeftValue >= currentRightValue) {
+          if (currentLeftValue <= currentRightValue) {
             input[inputIndex] = currentLeftValue;
             leftIndex++;
           }
--- src/test/java/sort/merge/MergeSortTest.java
+++ src/test/java/sort/merge/MergeSortTest.java
@@ -6,42 +6,35 @@ import org.junit.Test;
 public class MergeSortTest {
 
   @Test
-  public void Given_ANullArray_When_MergeSorting_Then_ShouldThrowNullPointerException() {
-    Assert.assertThrows(NullPointerException.class, () -> MergeSort.sort(null, true));
+  public void Given_ANullArray_When_Sorting_Then_ShouldThrowNullPointerException() {
+    Assert.assertThrows(NullPointerException.class, () -> MergeSort.sort(null));
   }
 
   @Test
-  public void Given_ASingleInteger_When_MergeSorting_Then_ShouldGetInteger() {
+  public void Given_ASingleInteger_When_Sorting_Then_ShouldGetInteger() {
     int[] input = new int[] { 1 };
-    MergeSort.sort(input, true);
+    MergeSort.sort(input);
     Assert.assertArrayEquals(new int[] { 1 }, input);
   }
 
   @Test
-  public void Given_Integers10To1_When_MergeSortingInAscendingOrder_Then_ShouldGetIntegers1To10() {
+  public void Given_Integers10To1_When_Sorting_Then_ShouldGetIntegers1To10() {
     int[] input = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-    MergeSort.sort(input, true);
+    MergeSort.sort(input);
     Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, input);
   }
 
   @Test
-  public void Given_Integers1To10_When_MergeSortingInDescendingOrder_Then_ShouldGetIntegers10To1() {
+  public void Given_Integers1To10_When_Sorting_Then_ShouldGetIntegers1To10() {
     int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    MergeSort.sort(input, false);
-    Assert.assertArrayEquals(new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, input);
-  }
-
-  @Test
-  public void Given_Integers1To10_When_MergeSortingInAscendingOrder_Then_ShouldGetIntegers1To10() {
-    int[] input = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    MergeSort.sort(input, true);
+    MergeSort.sort(input);
     Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, input);
   }
 
   @Test
-  public void Given_10IntegersInRandomOrder_When_MergeSortingInAscendingOrder_Then_ShouldRearrangeIntegersInAscendingOrder() {
+  public void Given_10IntegersInRandomOrder_When_Sorting_Then_ShouldGetIntegers1To10() {
     int[] input = new int[] { 10, 1, 9, 2, 8, 3, 7, 4, 6, 5 };
-    MergeSort.sort(input, true);
+    MergeSort.sort(input);
     Assert.assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, input);
   }
 
