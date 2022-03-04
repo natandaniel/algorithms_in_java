66674d5bdd79baaa71c5f69d9dcdf34f400c9b69
natandaniel <natan.daniel0@gmail.com>
2022-03-04 15:30:41 +0100

    updating test suite
    Change-Id: I2488ced3911bff8bb3c0c86e3ffbdc4a6ca93aae

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -2,13 +2,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import addition.binary.BinaryAdditionTest;
 import search.linear.LinearSearchTest;
 import sort.insertion.InsertionSortTest;
 import sort.merge.MergeSortTest;
 import sort.selection.SelectionSortTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ LinearSearchTest.class, SelectionSortTest.class, InsertionSortTest.class, MergeSortTest.class })
+@SuiteClasses({ BinaryAdditionTest.class, LinearSearchTest.class, SelectionSortTest.class, InsertionSortTest.class, MergeSortTest.class })
 public class TestSuite {
 
 }
